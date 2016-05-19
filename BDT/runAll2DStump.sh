a5c9e0c7521ec15ce23bc8879cc4879004eb9d70
Ben Nissan <bnissa01@login001.lux.tufts.edu>
2016-05-19 08:42:19 -0400

    Ran BDS and BDT

--- BDT/runAll2DStump.sh
+++ BDT/runAll2DStump.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python runbdtstump.py linearhardodds.arff linearhardevens.arff Results/linearhard0519
-python runbdtstump.py linearsoftodds.arff linearsoftevens.arff Results/linearsoft0519
-python runbdtstump.py circlehardodds.arff circlehardevens.arff Results/circlehard0519
-python runbdtstump.py circlesoftodds.arff circlesoftevens.arff Results/circlesoft0519
-python runbdtstump.py twocirclehardodds.arff twocirclehardevens.arff Results/twocirclehard0519
-python runbdtstump.py twocirclesoftodds.arff twocirclesoftevens.arff Results/twocirclesoft0519
+python runbdtstump.py linearhardodds.arff linearhardevens.arff ResultsStump/linearhard0519
+python runbdtstump.py linearsoftodds.arff linearsoftevens.arff ResultsStump/linearsoft0519
+python runbdtstump.py circlehardodds.arff circlehardevens.arff ResultsStump/circlehard0519
+python runbdtstump.py circlesoftodds.arff circlesoftevens.arff ResultsStump/circlesoft0519
+python runbdtstump.py twocirclehardodds.arff twocirclehardevens.arff ResultsStump/twocirclehard0519
+python runbdtstump.py twocirclesoftodds.arff twocirclesoftevens.arff ResultsStump/twocirclesoft0519
