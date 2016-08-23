4c947af02559b130b80a5c4ead9d159c4202157a
Ben Nissan <bnissa01@login001.lux.tufts.edu>
2016-08-23 12:04:11 -0400

    Removed failed 0809 run files and extraneous files

---
+++ Generators/twocirclehardgen.py
@@ -0,0 +1,24 @@
+import random
+
+size = 200000
+table = [[0 for i in range(3)] for i in range(size)] 
+
+for i in range(size):
+	f1 = random.uniform(0, 10)
+	f2 = random.uniform(0, 10)
+	if (f1 - 2) ** 2 + (f2 - 2) ** 2 <= 4 or (f1 - 7) ** 2 + (f2 - 7) ** 2 <= 4:
+		table[i][0] = "+1"
+	else:
+		table[i][0] = "-1"
+	table[i][1] = f1
+	table[i][2] = f2
+
+with open('TwoCircleHardTraining', 'w') as training, open('TwoCircleHardTestingAll', 'w') as testingall, open('TwoCircleHardTestingSignal', 'w') as testingsignal, open('TwoCircleHardTestingBackground', 'w') as testingbackground:
+	for i in range(0, size, 2):
+		training.write(table[i][0] + " 1:" + str(table[i][1]) + " 2:" + str(table[i][2]) + "\n")
+	for i in range(1, size, 2):
+		testingall.write(table[i][0] + " 1:" + str(table[i][1]) + " 2:" + str(table[i][2]) + "\n")
+		if table[i][0] == "+1":
+			testingsignal.write(table[i][0] + " 1:" + str(table[i][1]) + " 2:" + str(table[i][2]) + "\n")
+		else:
+			testingbackground.write(table[i][0] + " 1:" + str(table[i][1]) + " 2:" + str(table[i][2]) + "\n")
---
+++ Generators/twocirclesoftgen.py
@@ -0,0 +1,31 @@
+import random
+
+size = 200000
+table = [[0 for i in range(3)] for i in range(size)] 
+
+for i in range(size):
+	f1 = random.uniform(0, 10)
+	f2 = random.uniform(0, 10)
+	if (f1 - 2) ** 2 + (f2 - 2) ** 2 <= 4 or (f1 - 7) ** 2 + (f2 - 7) ** 2 <= 4:
+		Class = 1
+	else:
+		Class = -1
+	if 2.75 < (f1 - 2) ** 2 + (f2 - 2) ** 2 < 5.25 or 2.75 < (f1 - 7) ** 2 + (f2 - 7) ** 2 < 5.25:
+		if random.random() <= .15:
+			Class *= -1
+	if Class == 1:
+		table[i][0] = "+1"
+	else:
+		table[i][0] = "-1"
+	table[i][1] = f1
+	table[i][2] = f2
+
+with open('TwoCircleSoftTraining', 'w') as training, open('TwoCircleSoftTestingAll', 'w') as testingall, open('TwoCircleSoftTestingSignal', 'w') as testingsignal, open('TwoCircleSoftTestingBackground', 'w') as testingbackground:
+	for i in range(0, size, 2):
+		training.write(table[i][0] + " 1:" + str(table[i][1]) + " 2:" + str(table[i][2]) + "\n")
+	for i in range(1, size, 2):
+		testingall.write(table[i][0] + " 1:" + str(table[i][1]) + " 2:" + str(table[i][2]) + "\n")
+		if table[i][0] == "+1":
+			testingsignal.write(table[i][0] + " 1:" + str(table[i][1]) + " 2:" + str(table[i][2]) + "\n")
+		else:
+			testingbackground.write(table[i][0] + " 1:" + str(table[i][1]) + " 2:" + str(table[i][2]) + "\n")
